22d49d26fd259b2d81c69086b8010008b133f4a7
Jeanne Grenet <jeanne.grenet@gmail.com>
2025-05-20 09:32:13 +0200

    feat : manage color for pawn

--- src/strategy/Game.java
+++ src/strategy/Game.java
@@ -5,56 +5,59 @@ import java.util.Scanner;
 
 public class Game {
     private static boolean isPlaying = true;
-    public Game(){
+    private static int turn = 0;
+
+    public Game() {
         while (isPlaying) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Entrez une pièce et une position (ex: 'tour B3') :");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Entrez une pièce et une position (ex: 'tour B3') :");
 
-        String input = scanner.nextLine().trim().toLowerCase();
-        String[] parts = input.split(" ");
+            String input = scanner.nextLine().trim().toLowerCase();
+            String[] parts = input.split(" ");
 
-        if (parts.length == 1 && parts[0].equals("exit")) {
-            System.out.println("Fin du jeu");
-            isPlaying = false;
-            return;
-        }
-
-        if (parts.length != 2) {
-            System.out.println("Format invalide. Exemple attendu : 'tour B3'");
-            continue;
-        }
-
-        String piece = parts[0];
-        String position = parts[1].toUpperCase();
-
-        String column = position.substring(0, 1);
-        int row = Integer.parseInt(position.substring(1));
-
-
-        IPieceStrategy strategy = switch (piece) {
-            case "tour" -> new RookStrategy();
-            case "pion" -> new PawnStrategy();
-            case "cavalier" -> new KnightStrategy();
-            case "fou" -> new BishopStrategy();
-            case "roi" -> new KingStrategy();
-            case "dame" -> new QueenStrategy();
-            default -> {
-                System.out.println("Pièce inconnue : " + piece);
-                yield null;
+            if (parts.length == 1 && parts[0].equals("exit")) {
+                System.out.println("Fin du jeu");
+                isPlaying = false;
+                return;
             }
-        };
 
-        if (strategy == null) return;
+            if (parts.length != 2) {
+                System.out.println("Format invalide. Exemple attendu : 'tour B3'");
+                continue;
+            }
 
-        PieceContext context = new PieceContext(strategy);
+            String piece = parts[0];
+            String position = parts[1].toUpperCase();
 
-        List<String> moves = context.getAllowedPositions(column, row, Color.WHITE );
+            String column = position.substring(0, 1);
+            int row = Integer.parseInt(position.substring(1));
+            Color color = turn % 2 == 0 ? Color.WHITE : Color.BLACK;
 
-        System.out.println("Déplacements possibles pour la " + piece + " en " + position + " :");
-        for (String move : moves) {
-            System.out.print(move + " ");
-        }
-        System.out.println("\n");
+            IPieceStrategy strategy = switch (piece) {
+                case "tour" -> new RookStrategy();
+                case "pion" -> new PawnStrategy();
+                case "cavalier" -> new KnightStrategy();
+                case "fou" -> new BishopStrategy();
+                case "roi" -> new KingStrategy();
+                case "dame" -> new QueenStrategy();
+                default -> {
+                    System.out.println("Pièce inconnue : " + piece);
+                    yield null;
+                }
+            };
+
+            if (strategy == null) return;
+
+            PieceContext context = new PieceContext(strategy);
+
+            List<String> moves = context.getAllowedPositions(column, row, color);
+
+            System.out.println("Déplacements possibles pour la " + piece + " en " + position + " :");
+            for (String move : moves) {
+                System.out.print(move + " ");
+            }
+            System.out.println("\n");
+            turn++;
         }
     }
 }
